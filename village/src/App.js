3c5f0e3a360472687eac8b410704408ed2b40bf3
David Quiñones <daquinons@users.noreply.github.com>
2019-06-14 09:35:29 +0200

    Create POST request

--- village/src/App.js
+++ village/src/App.js
@@ -16,14 +16,26 @@ class App extends Component {
   }
 
   async componentDidMount() {
-    const smurfs = await this.getSmurfs()
-    this.setState({ smurfs:  smurfs});
+    const smurfs = await this.getSmurfs();
+    this.setState({ smurfs: smurfs });
   }
 
   async getSmurfs() {
-    const dataSmurfs = await axios.get(APIURL);
+    try {
+      const dataSmurfs = await axios.get(APIURL);
+      return dataSmurfs.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-    return dataSmurfs.data;
+  async createNewSmurf(smurf) {
+    try {
+      const dataResponse = await axios.post(APIURL, smurf);
+      this.setState({ smurfs: dataResponse.data });
+    } catch (error) {
+      console.log(error);
+    }
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
