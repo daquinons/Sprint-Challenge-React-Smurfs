1ac36d42cff61b100c00f1adead4742093321ece
David Quiñones <daquinons@users.noreply.github.com>
2019-06-14 09:26:32 +0200

    Display smurfs list

--- village/src/App.js
+++ village/src/App.js
@@ -1,15 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
-import './App.css';
-import SmurfForm from './components/SmurfForm';
-import Smurfs from './components/Smurfs';
+import "./App.css";
+import SmurfForm from "./components/SmurfForm";
+import Smurfs from "./components/Smurfs";
+
+const APIURL = "http://localhost:3333/smurfs";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      smurfs: [],
+      smurfs: []
     };
+  }
+
+  async componentDidMount() {
+    const smurfs = await this.getSmurfs()
+    this.setState({ smurfs:  smurfs});
+  }
+
+  async getSmurfs() {
+    const dataSmurfs = await axios.get(APIURL);
+
+    return dataSmurfs.data;
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
